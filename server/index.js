283bb19880bfec8607d1a06c6eb6d088338f985d
tomnisim <tomnisi@post.bgu.ac.il>
2023-05-30 12:54:34 +0300

    create index.js

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const cors = require("cors")
+const express = require("express")
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log(`Listening on port ${port}`))
